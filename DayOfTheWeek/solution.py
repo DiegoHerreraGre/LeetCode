813ea9755a97c2e4b2a91da86fcb2d5c67f86091
DiegoHerreraGre <diego.herrera.94@outlook.com>
2024-06-27 08:29:47 -0400

    Refactor Python code and add same functionality in Rust
    Removed an unnecessary semicolon at the end of a line in the Python solution for generating day of the week. Also added the same functionality written in Rust. Updated .idea/LeetCode.iml to include source and exclude folders, and added a default code style configuration.

--- DayOfTheWeek/solution.py
+++ DayOfTheWeek/solution.py
@@ -5,7 +5,7 @@ class Solution:
 
     def day_of_the_week(self, year: int, month: int, day: int) -> str:
         input_date = datetime.date(year, month, day)
-        weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+        weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
         return weekdays[input_date.weekday()]
 
 
---
+++ DayOfTheWeek/src/main.rs
@@ -0,0 +1,45 @@
+impl Solution {
+  pub fn day_of_the_week(day: i32, month: i32, year: i32) -> String {
+    let weekdays = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ];
+    let months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let is_leap_year = |x: i32| -> bool { x % 4 == 0 && (x % 100 != 0 || x % 400 == 0) };
+
+    let num_days = (1971..year)
+      .map(|y| 365 + is_leap_year(y) as i32)
+      .sum::<i32>()
+      + (0..month - 1)
+      .map(|m| months[m as usize] + (m == 1 && is_leap_year(year)) as i32)
+      .sum::<i32>()
+      + day;
+
+    // 01/01/1971 - Friday
+    let res_day = (4 + (num_days - 1)) % 7;
+    weekdays[res_day as usize].to_string()
+  }
+}
+
+/// The `Solution` struct represents a solution for a specific problem.
+///
+/// # Examples
+///
+/// ```rust
+/// let solution = Solution {};
+/// ```
+///
+/// # Safety
+/// This struct does not contain any unsafe code and is considered to be safe to use.
+struct Solution {}
+
+/// The `Solution` struct contains methods to calculate the day of the week.
+fn main() {
+  let sol = Solution {};
+  println!("{}", sol.day_of_the_week(15, 9, 2022));
+}
