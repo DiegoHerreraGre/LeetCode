ada655a661dfa35d2380d1bb4d08c68e7f74f4c4
DiegoHerreraGre <diego.herrera.94@outlook.com>
2024-06-28 07:23:35 -0400

    Change variable to constant in main function
    In the main function, the variable 'sol' was transformed into a constant 'SOL'. Additionally, the arguments passed to the 'day_of_the_week' function were modified to depict a different date.

--- DayOfTheWeek/src/main.rs
+++ DayOfTheWeek/src/main.rs
@@ -1,29 +1,29 @@
 impl Solution {
-  pub fn day_of_the_week(day: i32, month: i32, year: i32) -> String {
-    let weekdays = [
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-      "Sunday",
-    ];
-    let months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    let is_leap_year = |x: i32| -> bool { x % 4 == 0 && (x % 100 != 0 || x % 400 == 0) };
+    pub fn day_of_the_week(day: i32, month: i32, year: i32) -> String {
+        let weekdays = [
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday",
+        ];
+        let months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        let is_leap_year = |x: i32| -> bool { x % 4 == 0 && (x % 100 != 0 || x % 400 == 0) };
 
-    let num_days = (1971..year)
-      .map(|y| 365 + is_leap_year(y) as i32)
-      .sum::<i32>()
-      + (0..month - 1)
-      .map(|m| months[m as usize] + (m == 1 && is_leap_year(year)) as i32)
-      .sum::<i32>()
-      + day;
+        let num_days = (1971..year)
+            .map(|y| 365 + is_leap_year(y) as i32)
+            .sum::<i32>()
+            + (0..month - 1)
+                .map(|m| months[m as usize] + (m == 1 && is_leap_year(year)) as i32)
+                .sum::<i32>()
+            + day;
 
-    // 01/01/1971 - Friday
-    let res_day = (4 + (num_days - 1)) % 7;
-    weekdays[res_day as usize].to_string()
-  }
+        // 01/01/1971 - Friday
+        let res_day = (4 + (num_days - 1)) % 7;
+        weekdays[res_day as usize].to_string()
+    }
 }
 
 /// The `Solution` struct represents a solution for a specific problem.
@@ -40,6 +40,6 @@ struct Solution {}
 
 /// The `Solution` struct contains methods to calculate the day of the week.
 fn main() {
-  let sol = Solution {};
-  println!("{}", sol.day_of_the_week(15, 9, 2022));
+    const SOL: Solution = Solution {};
+    println!("{}", Solution::day_of_the_week(5, 1, 1994));
 }
