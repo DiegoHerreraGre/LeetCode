f85cb08fdee14249ef20eab9f779e4a092911486
Diego Herrera Gre <143101844+DiegoHerreraGre@users.noreply.github.com>
2024-08-23 21:11:43 -0400

    Added

---
+++ LongestCommonPrefix/solution.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} strs
+ * @return {string} 
+ */
+
+var longestCommonPrefix = function (strs) {
+    var result = '';
+    var splitStrings = strs.split('');
+    console.log(splitStrings);
+};
+
+longestCommonPrefix(["flower","flow","flight"])
